44b2f642615de9c16845359b275084f55e672d8e
wakens <65771520+wakens@users.noreply.github.com>
2020-05-29 12:25:28 -0400

    Update Die.java

--- Die.java
+++ Die.java
@@ -1,24 +1,26 @@
 import java.util.Random;
 
-// 
-// Decompiled by Procyon v0.5.36
-// 
-
 class Die
 {
+    // INSTANCE VARIABLES
     private int value;
     private Random rand;
     
+    // GETTER 
+    public int get() {
+        return this.value;
+    }
+    
+    // CONSTRUCTOR
     public Die() {
         this.value = 0;
         this.rand = new Random();
     }
-    
+   
+    /*
+     * This method sets the value to a random number between 1 and 6. 
+     */
     public void roll() {
         this.value = 1 + this.rand.nextInt(6);
-    }
-    
-    public int get() {
-        return this.value;
     }
 }
