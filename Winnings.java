5e31084c9956585e0df28ef4d198648a9eee85ee
wakens <65771520+wakens@users.noreply.github.com>
2020-05-29 11:55:00 -0400

    Update Winnings.java

--- Winnings.java
+++ Winnings.java
@@ -1,23 +1,32 @@
 import java.util.Arrays;
 
-// 
-// Decompiled by Procyon v0.5.36
-// 
-
 class Winnings
 {
+    // INSTANCE VARIABLES
     private int score;
     private int choice;
     static String[] WINSS;
     
+    /*
+	 * This is just a way to have all the variables inside be static.
+	 */
     static {
         Winnings.WINSS = new String[] { "QUIT", "YAHTZEE", "FULL HOUSE", "LARGE STRAIGHT", "SMALL STRAIGHT", "FOUR OF A KIND", "THREE OF A KIND", "A PAIR", "TWO PAIRS", "NUMBER OF 1'S", "NUMBER OF 2'S", "NUMBER OF 3'S", "NUMBER OF 4'S", "NUMBER OF 5'S", "NUMBER OF 6'S", "CHANCE" };
     }
     
+    // GETTERS
+    public int score() { return this.score; }
+    public int choice() { return this.choice; }
+    
+    // CONSTRUCTOR
     public Winnings() {
         this.score = 0;
     }
     
+    /*
+     * This method looks at the 5 dice numbers the user has gotten, checks for any special winning cases
+     * and adds the winnings to show the user what they've won.
+     */
     public void checkWinnings(final int[] aDice, final int[] wins) {
         this.choice = MiniYahtzee.w.option(Winnings.WINSS, "Which do you want to see if you have?");
         int x = 0;
@@ -137,13 +146,5 @@ class Winnings
             this.score = aDice[0] + aDice[1] + aDice[2] + aDice[3] + aDice[4];
             MiniYahtzee.w.msg("Your get " + this.score + " point(s).");
         }
-    }
-    
-    public int score() {
-        return this.score;
-    }
-    
-    public int choice() {
-        return this.choice;
     }
 }
